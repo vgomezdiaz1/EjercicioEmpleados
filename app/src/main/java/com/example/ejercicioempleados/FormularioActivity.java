856467a5db48aca5d82d1817c264bdeb90ca905a
vgomezdiaz1 <Telefonica04>
2022-10-21 18:12:03 +0200

    cositas añadidas

--- app/src/main/java/com/example/ejercicioempleados/FormularioActivity.java
+++ app/src/main/java/com/example/ejercicioempleados/FormularioActivity.java
@@ -3,9 +3,12 @@ package com.example.ejercicioempleados;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -15,34 +18,51 @@ public class FormularioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_formulario);
     }
     public void pulsacionBtnAceptar(View v){
-        if(comprobarVacios()){
-
-            EditText texto;
-            texto = findViewById(R.id.editTextNombre);
-            String tNombre = texto.getText().toString();
-            texto = findViewById(R.id.editTextApellidos);
-            String tApellido= texto.getText().toString();
-            texto = findViewById(R.id.editTextFormularioTelefono);
-            String tTelefono= texto.getText().toString();
-            texto = findViewById(R.id.editTextEdad);
-            String tEdad= texto.getText().toString();
-
+        int n = 0;
+        String tNombre ="";
+        String tApellido ="";
+        String tTelefono ="";
+        String tEdad ="";
+        EditText tNom = findViewById(R.id.editTextNombre);
+        EditText tApe = findViewById(R.id.editTextApellidos);
+        EditText tTelf = findViewById(R.id.editTextFormularioTelefono);
+        EditText tEd = findViewById(R.id.editTextEdad);
+        if(comprobarVacios(tEd)) {
+            tEdad = tEd.getText().toString();
+            n++;
+        }
+        if(comprobarVacios(tTelf)) {
+            tTelefono = tTelf.getText().toString();
+            n++;
+        }
+        if(comprobarVacios(tApe)){
+            tApellido = tApe.getText().toString();
+            n++;
+        }
+        if(comprobarVacios(tNom)) {
+            tNombre = tNom.getText().toString();
+            n++;
+        }
+        if(n==4){
             Intent i = new Intent(this,ConfirmacionDatosActivity.class);
-
             i.putExtra("nombre",tNombre);
             i.putExtra("apellido",tApellido);
             i.putExtra("telefono",tTelefono);
             i.putExtra("edad",tEdad);
             startActivity(i);
+        }else{
+            Toast.makeText(this,"Todos los campos son obligatorios", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private boolean comprobarVacios(){
-        EditText texto;
-        texto = findViewById(R.id.editTextNombre);
-        if(texto.getText().toString().equals("")){
-
+    private boolean comprobarVacios(EditText x){
+        if(x.getText().toString().equals("")){
+            x.requestFocus();
+            x.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.red)));
+            return false;
+        }else{
+            x.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.teal_700)));
+            return true;
         }
-        return true;
     }
 }
