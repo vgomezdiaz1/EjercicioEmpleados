8cb3a86345d8d3ec7bfb392ce9bb2f4fbd3eb35e
vgomezdiaz1 <Telefonica04>
2022-10-26 19:26:06 +0200

    Al hacer un cursor para leer los datos de un contacto, peta al hacer el cursor

--- app/src/main/java/com/example/ejercicioempleados/FormularioActivity.java
+++ app/src/main/java/com/example/ejercicioempleados/FormularioActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.database.Cursor;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
@@ -60,7 +62,6 @@ public class FormularioActivity extends AppCompatActivity {
 
     public void pulsacionBtnCargarContacto(View v) {
         Intent i = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-
         startActivityForResult(i, 34);
     }
 
@@ -71,6 +72,40 @@ public class FormularioActivity extends AppCompatActivity {
         if (requestCode == 34) {
             if (resultCode == RESULT_OK) {
                 Toast.makeText(this, "Todo ha ido bien y ha clicado ok", Toast.LENGTH_SHORT).show();
+                //Guardas el contacto en un Uri de Android no de java
+                Uri contactData = data.getData();
+                Cursor c = getContentResolver().query(contactData,null,null,null,null);
+                if(c.moveToFirst()){
+                    String whereName = ContactsContract.Data.MIMETYPE + " = ?";
+                    String[] whereNameParams = new String[] { ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE };
+                    Cursor nameCur = getContentResolver().query(ContactsContract.Data.CONTENT_URI, null, whereName, whereNameParams, ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
+                    while (nameCur.moveToNext()) {
+                        int i = nameCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
+                        int i2 =nameCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME);
+                        String given = nameCur.getString(i);
+                        String family = nameCur.getString(i2);
+
+                        EditText tNom = findViewById(R.id.editTextNombre);
+                        tNom.setText(given);
+
+                        EditText tApe = findViewById(R.id.editTextApellidos);
+                        tApe.setText(family);
+                    }
+                    nameCur.close();
+
+                    /*int i = c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_SOURCE);
+                    EditText tNom = findViewById(R.id.editTextNombre);
+                    tNom.setText(c.getString(i));
+                    int i1 = c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_SOURCE);
+                    EditText tApe = findViewById(R.id.editTextApellidos);
+                    tApe.setText(c.getString(i1));
+                    int i2 = c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER);
+                    EditText tTelf = findViewById(R.id.editTextFormularioTelefono);
+                    tTelf.setText(c.getString(i2));
+                    int i3 = c.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS);
+                    EditText tEd = findViewById(R.id.editTextEdad);
+                    tEd.setText(c.getString(i3));*/
+                }
             } else {
                 Toast.makeText(this, "Todo ha ido bien pero ha clicado cancel", Toast.LENGTH_SHORT).show();
             }
