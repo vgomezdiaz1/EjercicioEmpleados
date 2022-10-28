68ec4b3c661f6501bbf045bbd13248a38c7e996b
vgomezdiaz1 <Telefonica04>
2022-10-28 17:23:07 +0200

    Ya cogemos un contacto exacto y traemos el numero de telefono, email y nombre y apellidos por separado

--- app/src/main/java/com/example/ejercicioempleados/FormularioActivity.java
+++ app/src/main/java/com/example/ejercicioempleados/FormularioActivity.java
@@ -76,33 +76,64 @@ public class FormularioActivity extends AppCompatActivity {
                 Uri contactData = data.getData();
                 Cursor c = getContentResolver().query(contactData,null,null,null,null);
                 if(c.moveToFirst()){
-                    String whereName = ContactsContract.Data.MIMETYPE + " = ?";
-                    String[] whereNameParams = new String[] { ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE };
+                    //Aqui seleccionamos el id que ha cogido el usuario, sino como cogemos toda la agenda coge el primero de la agenda
+                    int idContactoQueQuiero = c.getColumnIndex(ContactsContract.Contacts._ID);
+                    String id = c.getString(idContactoQueQuiero);
+                    //Nos dice el tipo de dato que vamos a sacar oseasela consulta
+                    String whereName = ContactsContract.Data.MIMETYPE + " = ? AND " + ContactsContract.CommonDataKinds.StructuredName.CONTACT_ID + " = ? ";
+                    //Es un array porque nos puede devolver varias sentencias
+                    //Le ponemos el tipo de elemento que queremos extraer dentro de los {}, para igualar el tipo de dato con el whereName
+                    //En las dos datos que se pone aqui, se cambia por las interrogaciones de la consulta de mas arriba
+                    String[] whereNameParams = new String[] { ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE, id };
+                    //Hacemos el cuersor para el nombre
+                    //1º donde hace la busqueda el cursor (dentro de la agenda toda la informacion)
+                    //2º proyeccion a null
+                    //3º El tipo de datro que recibimos
+                    //4º Todos los datos que hemos sacado en la consulta
+                    //5º El orden a filtrar
                     Cursor nameCur = getContentResolver().query(ContactsContract.Data.CONTENT_URI, null, whereName, whereNameParams, ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
+                    //Mientras haya info en el cursor
                     while (nameCur.moveToNext()) {
+                        //Sacamos los dos indices que necesitamos
                         int i = nameCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
                         int i2 =nameCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME);
+                        //Guardamos en un String la posicion del cursor en la que estan los datos que queremos
                         String given = nameCur.getString(i);
                         String family = nameCur.getString(i2);
-
+                        //Añadimos a donde queremos los datos extraidos para mostrarlos
                         EditText tNom = findViewById(R.id.editTextNombre);
                         tNom.setText(given);
-
                         EditText tApe = findViewById(R.id.editTextApellidos);
                         tApe.setText(family);
                     }
                     nameCur.close();
-
-                    /*int i = c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_SOURCE);
-                    EditText tNom = findViewById(R.id.editTextNombre);
-                    tNom.setText(c.getString(i));
-                    int i1 = c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_SOURCE);
-                    EditText tApe = findViewById(R.id.editTextApellidos);
-                    tApe.setText(c.getString(i1));
-                    int i2 = c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER);
+                    //Con todo esto cogemos el telefono del contacto seleccionado
+                    String whereTelf = ContactsContract.CommonDataKinds.Phone.MIMETYPE + " = ? AND " + ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? ";
+                    String[] whereTelfParams = new String[] { ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE, id };
+                    Cursor telfCur = getContentResolver().query(ContactsContract.Data.CONTENT_URI, null, whereTelf, whereTelfParams, null);
+                    while (telfCur.moveToNext()) {
+                        int i = telfCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                        String given = telfCur.getString(i);
+                        EditText tTelf = findViewById(R.id.editTextFormularioTelefono);
+                        tTelf.setText(given);
+                    }
+                    telfCur.close();
+                    //Con todo esto cogemos el email del contacto seleccionado
+                    String whereMail = ContactsContract.CommonDataKinds.Email.MIMETYPE + " = ? AND " + ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ? ";
+                    String[] whereMailParams = new String[] { ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE , id };
+                    Cursor mailCur = getContentResolver().query(ContactsContract.Data.CONTENT_URI, null, whereMail, whereMailParams, null);
+                    while (mailCur.moveToNext()) {
+                        int i = mailCur.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS);
+                        String given = mailCur.getString(i);
+                        EditText tEd = findViewById(R.id.editTextEdad);
+                        tEd.setText(given);
+                    }
+                    telfCur.close();
+                    /*
+                    int i2 = c.getColumnIndex(ContactsContract.Contacts.Data.DATA4);
                     EditText tTelf = findViewById(R.id.editTextFormularioTelefono);
-                    tTelf.setText(c.getString(i2));
-                    int i3 = c.getColumnIndex(ContactsContract.CommonDataKinds.Email.ADDRESS);
+                    tTelf.setText(c.getString(i3));
+                    int i3 = c.getColumnIndex(ContactsContract.Contacts.Data.DATA4);
                     EditText tEd = findViewById(R.id.editTextEdad);
                     tEd.setText(c.getString(i3));*/
                 }
